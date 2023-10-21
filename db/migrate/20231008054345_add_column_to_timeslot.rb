1d45f17b5f096df0cbf7ee4fc02998290991ede3
ron029 <bonifacioronmark@gmail.com>
2023-10-21 09:44:59 +0800

    fix-bugs/time_slots

--- db/migrate/20231008054345_add_column_to_timeslot.rb
+++ db/migrate/20231008054345_add_column_to_timeslot.rb
@@ -1,6 +1,6 @@
 class AddColumnToTimeslot < ActiveRecord::Migration[6.1]
   def change
-    add_column :time_slot, :duration, :integer # Assuming you want to store the duration in minutes
-    add_column :time_slot, :interval, :integer # Assuming you want to store the interval in minutes
+    add_column :time_slots, :duration, :integer # Assuming you want to store the duration in minutes
+    add_column :time_slots, :interval, :integer # Assuming you want to store the interval in minutes
   end
 end
