178a50a374020c982faaaee1c0c4dd249fa13b73
ron029 <bonifacioronmark@gmail.com>
2023-10-21 09:24:41 +0800

    modify cors from local

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,9 +3,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://service-bundle-backend.vercel.app' # Replace with your frontend's URL
-    # origins 'http://localhost:8080'
     resource '/api/*',
-    # resource '*',
+      # origins 'http://localhost:8080'
+      # resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
