c132a3bcb1b4b343dab33e7de985e84400320090
ron029 <bonifacioronmark@gmail.com>
2023-10-21 09:55:34 +0800

    fix-bugs/timeslotmodel

--- db/migrate/20231008055539_add_duration_and_interval_to_time_slots.rb
+++
@@ -1,6 +0,0 @@
-class AddDurationAndIntervalToTimeSlots < ActiveRecord::Migration[6.1]
-  def change
-    add_column :time_slots, :duration, :integer
-    add_column :time_slots, :interval, :integer
-  end
-end
